ec1f04f054619b5b4980c9f4aa4316d604195333
Keara Harris <keara.s.harris@gmail.com>
2019-10-24 20:30:58 +0000

    Latest

--- db/migrate/20191024131232_devise_create_users.rb
+++ db/migrate/20191024131232_devise_create_users.rb
@@ -1,8 +1,9 @@
-# frozen_string_literal: tru ne
+# frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+
+      create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
