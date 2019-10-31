1e79e79cc7e6555f1953f3eca2cfe7e08ba3ed24
Keara Harris <keara.s.harris@gmail.com>
2019-10-31 14:37:23 +0000

    updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: Star Wars }, { name: Lord of the Rings }])
-#   Character.create(name: Luke, movie: movies.first)
+#   movies = Movie.create([{ (user: User.first, name:: Star Wars }, { (user: User.first, name:: Lord of the Rings }])
+#   Character.create(user: User.first, name:: Luke, movie: movies.first)
 
-Place.create!(name: 'Forever21', description: 'Plus-size section is located on the 4th level. A large selection of clothing for us Curvy Girls', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
-Place.create!(name: 'Ardene', description: 'Similar to Forever21, caters to a younger customer. A minimal selection of plus-size clothing but hey, at least they have some.', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
-Place.create!(name: 'Torrid', description: 'Everything is for us Curvy Girls. In addition to cute clothing, they offer a selection of shoes, that are wider widths, accessories, intimates,', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
-Place.create(name: 'Lane Bryant', description: 'Everything Curvy!, in addition the Cacique intimates store is attached to this location so you can find a large selection of bras, intimates, and shapewear', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
-Place.create!(name: 'Target', description: 'Plus-size clothing is located to the right of the mall entrance. Minimal selection of clothing for us Curvy Girls, but hey its target, you wont walk out empty-handed', address: '1000 Palisades Center Dr. West Nyack, NY 10960')
-Place.create!(name: 'Burlington', description: 'Small Plus-size section. Also offers a small junior plus selection, reasonable prices', address: '1000 Palisades Center Drive West Nyack, NY 10994')
-Place.create!(name: 'Macys', description: 'Plus-size located on the second floor. This location also has a Macys Backstage with a small Plus-size section as well.', address: '1000 Palisades Center Drive West Nyack, NY 10994')
-Place.create!(name: 'H and M', description: 'Yesss!!! this location actually has plus-sizes!. Small selection but there is a selection.', address: '1 Galleria Dr, Middletown, NY 10941')
-Place.create!(name: 'Fashion to Figure – Bay Plaza', description: ' Everything Curvy! Really trendy pieces, cute dresses and tops, and a great selection of accessories.', address: '5166 Kings Plaza, Brooklyn, NY 11234')
+Place.create!(user: User.first, name: 'Forever21', description: 'Plus-size section is located on the 4th level. A large selection of clothing for us Curvy Girls', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
+Place.create!(user: User.first, name: 'Ardene', description: 'Similar to Forever21, caters to a younger customer. A minimal selection of plus-size clothing but hey, at least they have some.', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
+Place.create!(user: User.first, name: 'Torrid', description: 'Everything is for us Curvy Girls. In addition to cute clothing, they offer a selection of shoes, that are wider widths, accessories, intimates,', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
+Place.create!(user: User.first, name: 'Lane Bryant', description: 'Everything Curvy!, in addition the Cacique intimates store is attached to this location so you can find a large selection of bras, intimates, and shapewear', address: '1000 Palisades Center Dr. West Nyack, NY 10994')
+Place.create!(user: User.first, name: 'Target', description: 'Plus-size clothing is located to the right of the mall entrance. Minimal selection of clothing for us Curvy Girls, but hey its target, you wont walk out empty-handed', address: '1000 Palisades Center Dr. West Nyack, NY 10960')
+Place.create!(user: User.first, name: 'Burlington', description: 'Small Plus-size section. Also offers a small junior plus selection, reasonable prices', address: '1000 Palisades Center Drive West Nyack, NY 10994')
+Place.create!(user: User.first, name: 'Macys', description: 'Plus-size located on the second floor. This location also has a Macys Backstage with a small Plus-size section as well.', address: '1000 Palisades Center Drive West Nyack, NY 10994')
+Place.create!(user: User.first, name: 'H and M', description: 'Yesss!!! this location actually has plus-sizes!. Small selection but there is a selection.', address: '1 Galleria Dr, Middletown, NY 10941')
+Place.create!(user: User.first, name: 'Fashion to Figure – Bay Plaza', description: ' Everything Curvy! Really trendy pieces, cute dresses and tops, and a great selection of accessories.', address: '5166 Kings Plaza, Brooklyn, NY 11234')
